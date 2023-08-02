ab5ac727c497aae31b4831f10ad0bd2d5423bfe4
ramala-prasanth <ramalaprasanthreddy@gmail.com>
2023-08-02 22:10:14 +0530

    final

--- index.js
+++ index.js
@@ -18,6 +18,39 @@ app.post('/addCustomerForm', (req, res) => {
     res.render('addCustomer');
 });
 
+app.post('/updateCustomer', (req, res) => {
+  var token = sessionstorage.getItem('token'); // Get the Bearer token from the request headers
+  // console.log(req.body.first_name);
+  var uuid = req.body.uuid;
+  const customerData = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    street: req.body.street,
+    address: req.body.address,
+    city: req.body.city,
+    state: req.body.state,
+    email: req.body.email,
+    phone: req.body.phone
+  };
+  // console.log(JSON.stringify(customerData));
+  // Call the API to create a new customer
+  var options = {
+    'method': 'POST',
+    'url': 'https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=update&uuid='+uuid,
+    'headers': {
+      'Authorization': 'Bearer '+token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(customerData)    
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+    // res.status(response.statusCode).send('Successfully Updated')
+    res.redirect('/update_displaycustomerlist');  
+  });  
+});
+
 app.post('/update', (req, res) => {
   var data = [req.body.uuid, req.body.first_name, req.body.last_name, req.body.street, req.body.address, req.body.city, req.body.state, req.body.email, req.body.phone]
   console.log(data);
@@ -25,20 +58,22 @@ app.post('/update', (req, res) => {
 });
 
 app.post('/delete', (req, res) => {
+  var token = sessionstorage.getItem('token'); // Get the Bearer token from the request headers
   var uuid = req.body.uuid;
   console.log(uuid);
   var options = {
     'method': 'POST',
     'url': 'https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=delete&uuid='+uuid,
     'headers': {
-      'Authorization': 'Bearer dGVzdEBzdW5iYXNlZGF0YS5jb206VGVzdEAxMjM= ',
+      'Authorization': 'Bearer '+token,
       'Cookie': 'JSESSIONID=A439E5F8A674E310B933045DA7EADA95'
     }
   };
   request(options, function (error, response) {
     if (error) throw new Error(error);
     console.log(response.body);
-    res.status(response.statusCode).send('Successfully Deleted')
+    // res.status(response.statusCode).send('Successfully Deleted')
+    res.redirect('/delete_displaycustomerlist');
   });
   
 
@@ -63,7 +98,7 @@ app.post('/addCustomer', (req, res) => {
       'method': 'POST',
       'url': 'https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=create',
       'headers': {
-        'Authorization': 'Bearer dGVzdEBzdW5iYXNlZGF0YS5jb206VGVzdEAxMjM= ',
+        'Authorization': 'Bearer '+token,
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(customerData)    
@@ -71,16 +106,19 @@ app.post('/addCustomer', (req, res) => {
     request(options, function (error, response) {
       if (error) throw new Error(error);
       console.log(response.body);
-      res.status(response.statusCode).send('Successfully Created')
+      res.redirect('/alert_displaycustomerlist');
+      // res.status(response.statusCode).send('Successfully Created')
+      // res.redirect('/displaycustomerlist');
     });    
   });
 
-app.get('/displaycustomerlist', (req, res) => {
+app.get('/update_displaycustomerlist', (req, res) => {
+  var token = sessionstorage.getItem('token'); // Get the Bearer token from the request headers
   var options = {
     'method': 'GET',
     'url': 'https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=get_customer_list',
     'headers': {
-      'Authorization': 'Bearer dGVzdEBzdW5iYXNlZGF0YS5jb206VGVzdEAxMjM='
+      'Authorization': 'Bearer '+token,
     },
   };
   request(options, function (error, response) {
@@ -88,7 +126,62 @@ app.get('/displaycustomerlist', (req, res) => {
     // console.log(response.body);
     var data = JSON.parse(response.body);
     // console.log(data);
-    res.render('customerlist', {data: data});
+    res.render('customerlist', {data: data,isAdded: false,isDeleted: false,isUpdated: true});
+  });
+});
+
+app.get('/delete_displaycustomerlist', (req, res) => {
+  var token = sessionstorage.getItem('token'); // Get the Bearer token from the request headers
+  var options = {
+    'method': 'GET',
+    'url': 'https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=get_customer_list',
+    'headers': {
+      'Authorization': 'Bearer '+token,
+    },
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    // console.log(response.body);
+    var data = JSON.parse(response.body);
+    // console.log(data);
+    res.render('customerlist', {data: data,isAdded: false,isDeleted: true,isUpdated: false});
+  });
+});
+
+
+app.get('/alert_displaycustomerlist', (req, res) => {
+  var token = sessionstorage.getItem('token'); // Get the Bearer token from the request headers
+  var options = {
+    'method': 'GET',
+    'url': 'https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=get_customer_list',
+    'headers': {
+      'Authorization': 'Bearer '+token,
+    },
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    // console.log(response.body);
+    var data = JSON.parse(response.body);
+    // console.log(data);
+    res.render('customerlist', {data: data,isAdded: true,isDeleted: false,isUpdated: false});
+  });
+});
+
+app.get('/displaycustomerlist', (req, res) => {
+  var token = sessionstorage.getItem('token'); // Get the Bearer token from the request headers
+  var options = {
+    'method': 'GET',
+    'url': 'https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=get_customer_list',
+    'headers': {
+      'Authorization': 'Bearer '+token,
+    },
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    // console.log(response.body);
+    var data = JSON.parse(response.body);
+    // console.log(data);
+    res.render('customerlist', {data: data,isAdded: false,isDeleted: false,isUpdated: false});
   });
   
 });
